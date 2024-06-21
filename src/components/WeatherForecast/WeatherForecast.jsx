bf871c688319adf58729a878b945fc6d499bb20b
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-06-21 09:56:17 -0400

    wrap jsx in function and export WeatherForecast

--- src/components/WeatherForecast/WeatherForecast.jsx
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -1,13 +1,19 @@
-<div className='weather'>
-	<h2>Day of the Week</h2>
-	<img
-		src=''
-		alt=''
-	/>
-	<p>
-		<span>conditions: </span>current weather conditions
-	</p>
-	<p>
-		<span>time: </span>time of day
-	</p>
-</div>;
+function WeatherForecast(props) {
+	return (
+		<div className='weather'>
+			<h2>Day of the Week</h2>
+			<img
+				src=''
+				alt=''
+			/>
+			<p>
+				<span>conditions: </span>current weather conditions
+			</p>
+			<p>
+				<span>time: </span>time of day
+			</p>
+		</div>
+	);
+}
+
+export default WeatherForecast;
